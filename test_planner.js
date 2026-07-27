c4597216ceb9649593111b2b34f9dffc0b12975b
Siyona Agarwal <siyona272010@gmail.com>
2026-07-27 13:07:57 +0000

    commit-7

--- test_planner.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config({ path: './server/.env' })
-const { validateItineraryInput, buildSystemPrompt } = require('./server/routes/planner')
-// oh wait, planner.js exports the router, not the functions.
